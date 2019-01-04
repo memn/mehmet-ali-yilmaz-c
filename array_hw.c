71ee0d88040e15578211d3632b9dc9a994bd0ce4
mehmet mumcu <m.e.mumcu@gmail.com>
2019-01-04 16:23:33 +0300

    odev

--- array_hw.c
+++ array_hw.c
@@ -3,3 +3,11 @@
 //
 
 #include "array_hw.h"
+
+
+void test() {
+    generateRandomMatrix(FILE, 0, 100);
+    readMatrixFromFile(FILE);
+    // matrix is filled
+
+}
--- array_hw.h
+++ array_hw.h
@@ -5,4 +5,28 @@
 #ifndef MEHMET_ALI_YILMAZ_C_ARRAY_HW_H
 #define MEHMET_ALI_YILMAZ_C_ARRAY_HW_H
 
+#define COLUMN_SIZE 9
+#define ROW_SIZE 9
+
+#define FILE "matrix.txt"
+
+int matrix[ROW_SIZE][COLUMN_SIZE];
+
+void generateRandomMatrix(char *filename, int randomMin, int randomMax); // only generate file, do not fill matrix!!
+
+void readMatrixFromFile(char *filename); // fill matrix
+
+int minInColumn(int column); // obvious
+
+int maxInColumn(int column);
+
+int minInRow(int row);
+
+int maxInRow(int row);
+
+int diagonalDiff();
+
+void test();
+
+
 #endif //MEHMET_ALI_YILMAZ_C_ARRAY_HW_H
